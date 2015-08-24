f9291a46ccaf7e1af1049f8317e37a791b61027a
Adriano Soares <gsoares.adriano@gmail.com>
2015-08-24 14:50:47 -0300

    action type in editor

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 var app = angular.module("editor", []);
 
-app.controller('editorController', ['$scope', function($scope) {
-    $scope.roomList = ["teste", "teste1"];
+app.controller('editorController', ['$scope', '$http', function($scope, $http) {
+    $scope.roomList = [];
+    $scope.events =  ["swipeUp", "swipeLeft", "swipeDown", "swipeRight"];
+    $scope.actions = ["pickItem", "gotoRoom"];
+    $http.get('Level1.json')
+     .then(function(res){
+        $scope.roomList = res.data;
+      });
+
+    $scope.loadJson = function () {
+
+    };
+    $scope.saveJson = function () {
+
+    };
+
     $scope.newRoom = function () {
-        $scope.roomList.push("wtf");
+        $scope.roomList.push({
+            "background": "",
+            "events": {}
+        } );
+    };
+
+    $scope.newEvent = function (room, eventType) {
+        room.events[eventType] = {
+            "action": ""
+        };
     };
 
 }]);
